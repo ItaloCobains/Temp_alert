85c2c0cb3aa3ee27519431a2ac9ce4a04d01b001
Italo Brandão <italobrandao.contato@gmail.com>
2025-10-27 14:56:23 -0300

    Refactor main.cpp to include DHT sensor functionality and update platformio.ini with upload port

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
+#include <DHT.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+#define LED_RED_PIN 11
+#define LED_YELLOW_PIN 12
+#define LED_GREEN_PIN 13
+#define MAX_TEMP 30.0
+#define MIN_HUMIDITY 10.0
+
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  dht.begin();
+
+  pinMode(LED_RED_PIN, OUTPUT);
+  pinMode(LED_YELLOW_PIN, OUTPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+
+  if (t > MAX_TEMP) {
+    digitalWrite(LED_RED_PIN, HIGH);
+    digitalWrite(LED_YELLOW_PIN, LOW);
+    digitalWrite(LED_GREEN_PIN, LOW);
+  } else if (h < MIN_HUMIDITY) {
+    digitalWrite(LED_RED_PIN, LOW);
+    digitalWrite(LED_YELLOW_PIN, HIGH);
+    digitalWrite(LED_GREEN_PIN, LOW);
+  } else {
+    digitalWrite(LED_RED_PIN, LOW);
+    digitalWrite(LED_YELLOW_PIN, LOW);
+    digitalWrite(LED_GREEN_PIN, HIGH);
+  }
 }
